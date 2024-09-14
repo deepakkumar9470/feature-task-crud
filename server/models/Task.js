e09f08612e264feb2fd64dadd65870bfdcbcf1e2
deepakkumar9470 <dk397787@gmail.com>
2024-09-14 10:37:27 +0530

    added models for authentication (user) and fro task crud (task)

---
+++ server/models/Task.js
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose';
+
+const TaskSchema = new mongoose.Schema({
+    title  : {
+        type: String,
+        required: true
+    },
+    desc  : {
+        type: String,
+        required: true
+    },
+    duedate  : {
+        type: Date,
+        default : Date.now
+
+    },
+    status : {
+        type:String,
+        enum: ['todo', 'inprogress', 'done'],
+        default: 'todo'    }
+  
+},{timestamps : true})
+
+
+const TaskModel = mongoose.model('Task',TaskSchema);
+
+export default TaskModel;
