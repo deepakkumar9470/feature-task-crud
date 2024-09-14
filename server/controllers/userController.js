888554f5eb05de0843c188123cff3016e040cfe9
deepakkumar9470 <dk397787@gmail.com>
2024-09-14 12:47:23 +0530

    add & generate jwt token during registration & sent to json

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,22 +1,39 @@
-import UserModel from '../models/User.js';
 import bcrypt from 'bcrypt';
+import generateJwtToken from '../utils/generateJwtToken.js';  
+import UserModel from '../models/User.js'; 
 
-export const userRegistration = async (req,res) =>{
-    const {name,email,password} = req.body;
-    if(!name || !email || !password){
-        res.status(400).json({ message: "Please enter all fields" });  
+/********** User Registration **********/
+export const userRegistration = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    return res.status(400).json({ message: "Please enter all fields" });
+  }
+
+  try {
+    const hashedPassword = await bcrypt.hash(password, 12);
+    const newUser = await UserModel.create({
+      name,
+      email,
+      password: hashedPassword
+    });
+
+    if (newUser) {
+      const token = generateJwtToken(res,newUser._id);
+
+      res.status(201).json({
+        message: "User Registered successfully.",
+        _id: newUser._id,
+        name: newUser.name,
+        email: newUser.email,
+        token
+      });
+    } else {
+      res.status(400).json({ message: 'Invalid user data' });
     }
-    try {
-        const hashedPassword = await  bcrypt.hash(password,12);
-        const newUser = await UserModel.create({
-            name,
-            email,
-            password:hashedPassword
-        })
-       await newUser.save();
-       res.status(201).json({ message: "User Registered successfully.." });
- 
-    } catch (error) {
-        res.status(500).json({ message: "Failed to user registration" });  
-    }
-}
+
+  } catch (error) {
+    console.error("Error during registration:", error);
+    res.status(500).json({ message: "Failed to register user" });
+  }
+};
