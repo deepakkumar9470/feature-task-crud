91b0b4d2545d035011c991a7d834d5b07e8c131f
deepakkumar9470 <dk397787@gmail.com>
2024-09-16 13:05:04 +0530

    install express asunc handler , add errormiddleware , and apply globally in index.js file for handling error with ease

--- server/index.js
+++ server/index.js
@@ -5,29 +5,34 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
+import {notFound,errorHandler} from './middleware/errorMiddleware.js'
 import connectDatabase from './db/db.js';
 import authRoute from './routes/auth.js';
 import taskRoute from './routes/task.js';
 
-app.use(express.json())
-app.use(cookieParser())
+app.use(express.json());
+app.use(cookieParser());
 app.use(express.urlencoded({extended : true}))
 app.use(cors({
     origin: 'http://localhost:3000', 
     credentials: true,
   }));
 
-app.use('/api/user', authRoute)
-app.use('/api/task', taskRoute)
+app.use('/api/user', authRoute);
+app.use('/api/task', taskRoute);
 
-connectDatabase()
+connectDatabase();
 
 app.get('/', (req,res)=>{
-    res.send('hello mern auth..')
+    res.send('hello task management..')
 })
 
+// Handling global error
+app.use(notFound);
+app.use(errorHandler);
+
 app.listen(PORT, ()=>{
-    console.log(`Server started at port http://localhost:${PORT}`)
+    console.log(`Server started at port http://localhost:${PORT}`);
 });
 
 
